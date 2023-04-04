9c9e89fa39624b2eb10cabda1658b481ec52d70e
ikromyusuf <ikromyusupov09@gmail.com>
2023-04-04 17:15:07 +0500

    done

--- file03.py
+++ file03.py
@@ -1,10 +1,10 @@
 def main(data:str):
     """
-    The data is from the file. Return the numbers as a list type.
+    The data is from the file. Return the digits as a list type.
     Args:
         data: str
     Returns:
         list: return answer
     """
-    
+
 # Read data from file
